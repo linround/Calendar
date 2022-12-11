330847bd43776ca3fa2245a8f50f88b729e3ae7d
linyuan <673609957@qq.com>
2022-12-11 20:37:20 +0800

    feature: Router module

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import './App.css'
-import {
-  Route, Routes, Link
-} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './view/Home'
 import Calendar from './view/Calendar'
+import Buttons from './view/Buttons'
 
 function App() {
 
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Calendar">Calendar</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/Calendar" element={<Calendar />}></Route>
+        <Route path="/Buttons" element={<Buttons />}></Route>
       </Routes>
     </>
   )
--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom'
+import menuStyles from '@/src/styles/menu.module.less'
+
 export default function () {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <Link to='/Calendar' target='_blank'>
+        <div className={menuStyles.menuContainer}>
+          Calendar
+        </div>
+      </Link>
+      <Link to='/Buttons' target='_blank'>
+        <div className={menuStyles.menuContainer}>
+          Buttons
+        </div>
+      </Link>
+    </>
+
   )
 }
