7562ebdfccb9e40448425e70adcebf9be239b8be
linyuan <673609957@qq.com>
2023-03-06 11:00:49 +0800

    feature：日历ui优化

--- src/view/LCalendar/SideComponent/CreateCalendar.tsx
+++ src/view/LCalendar/SideComponent/CreateCalendar.tsx
@@ -66,6 +66,7 @@ export function CreateCalendar(props: IProps) {
             <CirclePicker
               onChange={handleChange}
               color={color}
+              circleSpacing={0}
               width='100%'
               colors = {['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4',
                 '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FF9800', '#FF5722', '#795548', '#607D8B']} />
