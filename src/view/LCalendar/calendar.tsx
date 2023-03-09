2beedd4fea64edb5d2735c60e0fd6d33cbb82097
linyuan <673609957@qq.com>
2023-03-09 19:13:50 +0800

    feature：添加404路由

--- src/view/LCalendar/calendar.tsx
+++ src/view/LCalendar/calendar.tsx
@@ -1,9 +1,17 @@
 import LCalendar from './index'
 import { VisualContext } from './props/VisualContext'
+import { useAppSelector } from '../../store/hooks'
+import { selectUser } from '../../store/features/user/userSlice'
+import { Navigate } from 'react-router-dom'
+
 export function CalendarPage() {
+  const user = useAppSelector(selectUser)
   return (
-    <VisualContext value={{}}>
-      <LCalendar />
-    </VisualContext>
+    <>
+      {!user && <Navigate to='/' />}
+      {user && <VisualContext value={{}}>
+        <LCalendar />
+      </VisualContext>}
+    </>
   )
 }
