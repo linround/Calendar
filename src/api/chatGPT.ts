5267d0757917a19908b836b6618c4e6bcc4fbce1
linyuan <673609957@qq.com>
2023-03-26 13:15:52 +0800

    feature：接口重构

--- src/api/chatGPT.ts
+++ src/api/chatGPT.ts
@@ -2,19 +2,25 @@ import request from '../request'
 import { generateChatMessage } from '../utils/chatGPT'
 const domain = 'https://api.openai.com'
 
-const createOpenaiRequest = function (url:string, input:any) {
+const createOpenaiRequest = function (url:string) {
   const api = `${domain}${url}`
-  return request<any, any>({
-    url: api,
-    method: 'POST',
-    data: input,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer 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',
-    },
-  })
+  return function (input:any) {
+    return  request<any, any>({
+      url: api,
+      method: 'POST',
+      data: input,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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',
+      },
+    })
+  }
 }
+const chatRequest = createOpenaiRequest('/v1/chat/completions')
+
+
+
 
 export function handleSendChatMessage(input:string) {
-  return createOpenaiRequest('/v1/chat/completions', generateChatMessage(input))
+  return chatRequest(generateChatMessage(input))
 }
