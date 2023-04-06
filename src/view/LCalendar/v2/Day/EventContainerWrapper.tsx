73c2021833b4d670f7cfc4398c5c9396f1dd2bc7
linyuan <673609957@qq.com>
2023-04-06 16:28:02 +0800

    feature：关于渲染事件的相关逻辑

--- src/view/LCalendar/v2/Day/EventContainerWrapper.tsx
+++ src/view/LCalendar/v2/Day/EventContainerWrapper.tsx
@@ -1,22 +1,29 @@
-import React, { createRef, ReactElement } from 'react'
+import React, {
+  createRef, ReactElement, useState
+} from 'react'
 import { ISlotMetrics } from '../utils/timeSlots'
 
 interface IEventContainerWrapper {
   slotMetrics:ISlotMetrics
 }
 export function EventContainerWrapper(props:React.PropsWithChildren<IEventContainerWrapper>) {
-  const { children, } = props
-  // 事件容器的子节点
-  const events = (children as ReactElement).props.children
+
   const ref = createRef<HTMLDivElement>()
 
-  const Content = () => React.cloneElement(children as ReactElement, {
-    children: (
-      <React.Fragment>
-        {events}
-      </React.Fragment>
-    ),
-  })
+  const Content = () => {
+    const { children, slotMetrics, } = props
+    // 事件容器的子节点
+    const events = (children as ReactElement).props.children
+    return (
+      React.cloneElement(children as ReactElement, {
+        children: (
+          <React.Fragment>
+            {events}
+          </React.Fragment>
+        ),
+      })
+    )
+  }
   return (
     <div ref={ref}>
       <Content />
