58488eff3b63146546ff01b2c8ec02296dd2ec61
linyuan <673609957@qq.com>
2023-05-08 12:37:44 +0800

    CHORE:线条处理

--- src/view/About/index.tsx
+++ src/view/About/index.tsx
@@ -90,6 +90,91 @@ function canvasClip(context:CanvasRenderingContext2D) {
 
 
 }
+
+function canvasScale(context:CanvasRenderingContext2D) {
+// scale 根据x水平方向和y轴垂直线方向，位canvas单位添加缩放
+//   例如：将0.5作为缩小银子最终单位会变成0.5图像素，形状尺寸会变成原来的一半
+//   将2.0作为缩放因子，将会增大单位尺寸变成两个像素。形状的尺寸将会变成原来的两倍
+  context.save()
+  context.setTransform(
+    1, 0, 0, 1, 0, 0
+  )
+  context.fillRect(
+    100, 100, 200, 200
+  )
+  context.fillStyle = '#1b619e'
+  // context.setTransform(
+  //   0.5, 0, 0, 0.5, 0, 0
+  // )
+  // 由于左边点都已经进行了缩放，所以坐标点之间的差值就会被缩放，从而导致，坐标点的距离被缩放
+  context.scale(0.5, 0.5)
+  context.fillRect(
+    100, 100, 100, 200
+  )
+  context.restore()
+  context.filter = 'blur(0)'
+  context.font = '48px serif'
+  // 文本底部的坐标是y，文本开始的坐标是x
+  context.fillText(
+    'Hello world', 0, 100, 50
+  )
+  // 这个方法通过将指定区域设置位透明的方式从而对该区域进行擦除操作
+  context.clearRect(
+    100, 100, 100, 100
+  )
+}
+function canvasSetTransform(context:CanvasRenderingContext2D) {
+  // setTransform使用单位矩阵重新设置当前的变换并调用变换的方法
+  // 这个方法不会覆盖当前的变换矩阵，会多次叠加变换
+
+  // a 水平缩放 c 水平倾斜 e 水平移动
+  // b 垂直倾斜 d 垂直缩放 f 垂直移动
+  //     1        1        0
+
+  // context.fillStyle = '#FFA500'
+  // context.fillRect(
+  //   0, 0, 280, 220
+  // )
+  // context.fillStyle = 'red'
+  // context.fillRect(
+  //   0, 0, 100, 100
+  // // )
+  context.setTransform(
+    2, 0.2, 0.8, 2, 0, 0
+  )
+  context.fillStyle = 'blue'
+  // save 将绘制状态保存到栈中
+  // 绘制状态的组成:
+  // 当前的变换矩阵
+  // 当前的剪切区域
+  context.save()
+  context.setTransform(
+    1, 0, 0, 1, 0, 0
+  )
+  // x轴方向 width = 100*2+100*0.8=280
+  // y轴方向 height = 100*2 + 100*0.2=220
+  // 在垂直方向发生倾斜，即y周坐标发生变化  注意倾斜和平移的是有区别的，坐标零点发生倾斜，还是坐标零点；
+  // 但是普通坐标点发生倾斜，坐标点就会发生变化
+  context.fillStyle = 'green'
+  context.fillRect(
+    0, 0, 100, 100
+  )
+  // restore 将canvas恢复到最近的保存状态
+  context.restore()
+  context.fillRect(
+    150, 40, 100, 100
+  )
+}
+function canvasSave(ctx:CanvasRenderingContext2D) {
+  // save的目的只有保存当前状态的作用，从而便捷的恢复到这个状态
+  ctx.setTransform(
+    2, 0, 0, 2, 0, 0
+  )
+  ctx.save()
+  ctx.fillRect(
+    100, 100, 100, 100
+  )
+}
 export class About extends React.Component<AppProps, AppState> {
   constructor(props:AppProps) {
     super(props)
@@ -102,48 +187,11 @@ export class About extends React.Component<AppProps, AppState> {
   canvasRef(canvas:HTMLCanvasElement) {
     if (!canvas) return
     const context = canvas.getContext('2d')!
-    // setTransform使用单位矩阵重新设置当前的变换并调用变换的方法
-    // 这个方法不会覆盖当前的变换矩阵，会多次叠加变换
-
-    // a 水平缩放 c 水平倾斜 e 水平移动
-    // b 垂直倾斜 d 垂直缩放 f 垂直移动
-    //     1        1        0
-
-    // context.fillStyle = '#FFA500'
-    // context.fillRect(
-    //   0, 0, 280, 220
-    // )
-    // context.fillStyle = 'red'
-    // context.fillRect(
-    //   0, 0, 100, 100
-    // // )
-    context.setTransform(
-      2, 0.2, 0.8, 2, 0, 0
-    )
-    context.fillStyle = 'blue'
-    // save 将绘制状态保存到栈中
-    // 绘制状态的组成:
-    // 当前的变换矩阵
-    // 当前的剪切区域
-    canvasClip(context)
+    // canvasSetTransform(context)
+    // canvasClip(context)
     // 当前的虚线列表
-    context.save()
-    context.setTransform(
-      1, 0, 0, 1, 0, 0
-    )
-    // x轴方向 width = 100*2+100*0.8=280
-    // y轴方向 height = 100*2 + 100*0.2=220
-    // 在垂直方向发生倾斜，即y周坐标发生变化  注意倾斜和平移的是有区别的，坐标零点发生倾斜，还是坐标零点；
-    // 但是普通坐标点发生倾斜，坐标点就会发生变化
-    context.fillStyle = 'green'
-    context.fillRect(
-      0, 0, 100, 100
-    )
-    // restore 将canvas恢复到最近的保存状态
-    context.restore()
-    context.fillRect(
-      150, 40, 100, 100
-    )
+    // canvasScale(context)
+    canvasSave(context)
   }
   render() {
     const { width, height, } = this.state
