62775c983f8ba4c99b5fb72e3b6917bf255cb9d7
linyuan <673609957@qq.com>
2023-05-09 15:05:10 +0800

    CHORE:svg

--- src/view/About/SVG.tsx
+++ src/view/About/SVG.tsx
@@ -80,16 +80,44 @@ function Path() {
 
       求得第二个命令的第一个控制点）
       */}
-      <path d="M 10 100 C 50 10, 150 10, 200 100 S 350 190 400 100" strokeWidth={5} stroke="yellow" fill="transparent"/>
-      <circle cx={10} cy={100} r={10} fill={'blue'}  />
-      <circle cx={50} cy={10} r={10} fill={'blue'}  />
-      <circle cx={150} cy={10} r={10} fill={'blue'}  />
-      <circle cx={200} cy={100} r={10} fill={'blue'}  />
+      {/*<path d="M 10 100 C 50 10, 150 10, 200 100 S 350 190 400 100" strokeWidth={5} stroke="yellow" fill="transparent"/>*/}
+      {/*<circle cx={10} cy={100} r={10} fill={'blue'}  />*/}
+      {/*<circle cx={50} cy={10} r={10} fill={'blue'}  />*/}
+      {/*<circle cx={150} cy={10} r={10} fill={'blue'}  />*/}
+      {/*<circle cx={200} cy={100} r={10} fill={'blue'}  />*/}
 
 
-      <circle cx={250} cy={190} r={10} fill={'blue'}  />
-      <circle cx={350} cy={190} r={10} fill={'blue'}  />
-      <circle cx={400} cy={100} r={10} fill={'blue'}  />
+      {/*<circle cx={250} cy={190} r={10} fill={'blue'}  />*/}
+      {/*<circle cx={350} cy={190} r={10} fill={'blue'}  />*/}
+      {/*<circle cx={400} cy={100} r={10} fill={'blue'}  />*/}
+
+
+
+
+
+
+      {/*使用Q来给定一个控制点，
+      用来确定起点和终点的曲线斜率
+
+       观察以下的结果可以得知，就算是得知了初始的曲线斜率，相比知道初始的控制点，还是会发生不一致的问题;
+       因为Q创建的是二次贝塞尔曲线啊
+      */}
+
+      <path d="M 20 150 Q 50 50, 80 150" strokeWidth={5} stroke="black" fill="transparent"/>
+      <path d="M 20 150 C 35 100, 65 100, 80 150" strokeWidth={5} stroke="red" fill="transparent"/>
+      <circle cx={20} cy={150} r={10} fill={'blue'}  />
+      <circle cx={35} cy={100} r={10} fill={'blue'}  />
+      <circle cx={65} cy={100} r={10} fill={'blue'}  />
+      <circle cx={80} cy={150} r={10} fill={'blue'}  />
+
+      <circle cx={50} cy={50} r={10} fill={'blue'}  />
+
+
+
+
+
+
+
     </svg>
   )
 }
