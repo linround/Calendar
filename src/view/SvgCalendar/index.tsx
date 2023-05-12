dfbd3c0af933531455da41596b440baf13757e60
linyuan <673609957@qq.com>
2023-05-12 20:16:53 +0800

    feature:SVGCalendar

--- src/view/SvgCalendar/index.tsx
+++ src/view/SvgCalendar/index.tsx
@@ -1,22 +1,61 @@
+
+const centerX = 0
+const centerY = 0
+const pageWidth = 1000
+const pageHeight = 1000
+const radius = 10
+const strokeWidth = 2
+
+const leftWidth = 200
+const rightWidth = pageWidth - leftWidth
+
 export function SvgCalendar() {
   // viewBox 的含义
   // min-x 重新定义零点x
   // min-y 重新定义零点y
   // width 定义宽度单位
   // height 定义高度单位
-  const radius = 10
   return (
-    <svg viewBox={'-500 -500 1000 1000'}
-      xmlns="http://www.w3.org/2000/svg" >
-      <rect x={0} y={0} width={100} height={100} fill={'blue'}></rect>
-      <circle cx={0} cy={0} r={radius} fill={'red'}></circle>
-      <circle cx={500} cy={0} r={radius} fill={'black'}></circle>
-      <circle cx={-500} cy={0} r={radius} fill={'black'}></circle>
-      <circle cx={0} cy={500} r={radius} fill={'black'}></circle>
-      <circle cx={0} cy={-500} r={radius} fill={'black'}></circle>
-      <g>
-        <rect x={0} y={0} ></rect>
-      </g>
-    </svg>
+    <div style={{
+      height: '100%',
+      width: '100%',
+      overflow: 'auto',
+    }}>
+      <svg viewBox={`${centerX} ${centerY} ${pageWidth} ${pageHeight}`}
+        xmlns="http://www.w3.org/2000/svg" >
+        {/*绘制中心点坐标*/}
+        <circle cx={centerX} cy={centerY} r={radius} fill={'red'}></circle>
+        {/*绘制左上角坐标*/}
+        <circle cx={centerX} cy={centerY} r={radius} fill={'black'}></circle>
+        {/*绘制右上角*/}
+        <circle cx={pageWidth} cy={centerY} r={radius} fill={'black'}></circle>
+        {/*绘制左下角坐标*/}
+        <circle cx={0} cy={pageHeight} r={radius} fill={'black'}></circle>
+        {/*绘制右下角坐标*/}
+        <circle cx={pageWidth} cy={pageHeight} r={radius} fill={'black'}></circle>
+        {/*绘制整个矩形框*/}
+        <g>
+          <rect x={centerX} y={centerY} width={pageWidth} height={pageHeight} fill={'none'} strokeWidth={strokeWidth} stroke={'blue'} ></rect>
+        </g>
+        <SvgCalendarLeft />
+        <SvgCalendarRight />
+      </svg>
+    </div>
+  )
+}
+function SvgCalendarLeft() {
+  // 绘制左边区域
+  return (
+    <g>
+      <rect x={centerX} y={centerY} width={leftWidth} height={pageHeight} fill={'none'} strokeWidth={strokeWidth} stroke={'red'}></rect>
+    </g>
+  )
+}
+function SvgCalendarRight() {
+  // 绘制右边区域
+  return (
+    <g>
+      <rect x={leftWidth} y={centerY} width={rightWidth} height={pageHeight} fill={'none'} strokeWidth={strokeWidth} stroke={'blue'}></rect>
+    </g>
   )
 }
