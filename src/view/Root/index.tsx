bb737bd986b9ce083c2b06e2096e7421bec65d83
linyuan <yuanlincuc@gmail.com>
2024-03-29 10:47:54 +0800

    refactor: add monitor

--- src/view/Root/index.tsx
+++ src/view/Root/index.tsx
@@ -1,8 +1,28 @@
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+
+import { addRecords, getRecords } from '@linround/commonapi'
+
+export const RootPage = function () {
+  useEffect(() => {
+    console.log('RootPage')
+    getRecords()
+      .then((response:any) => {
+        console.log('getRecords', response)
+      })
+  }, [])
+
+  useEffect(() => {
+    addRecords()
+      .then((response:any) => {
+        console.log('addRecords', response)
+      })
+  }, [])
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
 
 
-export const RootPage = () => (
-  <>
-    <Outlet />
-  </>
-)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+declare module '@linround/commonapi' {
+  export function getRecords(): Promise<any>
+  export function addRecords(): Promise<any>
+}
